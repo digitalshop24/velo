3803942569baa654793ad30e41465327d0855e5c
K. Kanyuka <kkan.web@gmail.com>
2015-12-07 15:39:52 +0300

    similar func fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ module Product
   def self.set_preview(obj, image_id)
     obj.update(image: Image.find(image_id).image)
   end
-  def similar()
-    self.class.where(:hit => "true").order("RANDOM()").limit(4)
+  def similar(*features)
+    self.class.where(hit: true).order("RANDOM()").limit(4)
   end
 end
